953963dfae47027e2c99966a6fbd15b648d49c10
Pavanjethewad <pavanjethewad2@gmail.com>
2022-11-03 18:10:59 +0530

    commit

--- JavaScript/DAY004(29-10-2022)/index.js
+++ JavaScript/DAY004(29-10-2022)/index.js
@@ -24,3 +24,39 @@ console.log(typeof(nulla))
 
 let nulla1 = null
 console.log(typeof(nulla1))
+
+// Operators
+// 1) assignment operators
+// Use to assign values
+//    = ,+= , -= ,/= , *= , %= , **=
+var a = 4
+console.log("=  ------>",a);
+
+a += 5
+console.log("+=   ====>",a);
+
+a -= 5
+console.log("-=   ====>",a);
+
+a /= 5
+console.log("/=   ====>",a);
+
+a %= 5
+console.log("%=   ====>",a);
+
+a *= 5
+console.log("+=   ====>",a);
+
+// comparison Operators
+// == , != , <= , >= , < , >
+var x =10
+var y =10
+var z ="10"
+var w =     1
+console.log(x==y)
+console.log(x!=y)   
+console.log(x===y)
+console.log(x===z)
+console.log(x<w)
+console.log(x>w)
+
