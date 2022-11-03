79f1af90de993907b2566df344cc51497d0fb927
Pavanjethewad <pavanjethewad2@gmail.com>
2022-11-03 17:31:24 +0530

    day 4 js commited

---
+++ JavaScript/DAY004(29-10-2022)/index.js
@@ -0,0 +1,26 @@
+var a = 10
+var b = 2
+console.log(a+b)
+console.log(a-b)
+console.log(a*b)
+console.log(a%b)
+console.log(a/b)
+console.log(a ** b) //power 
+console.log(a ^ b) //EXor 
+
+
+// Data Types
+const number = 3
+console.log(typeof(number))
+
+const string  = "pavan"
+console.log(typeof(string))
+
+var shocked 
+console.log(typeof(shocked))
+
+const nulla = null
+console.log(typeof(nulla))
+
+let nulla1 = null
+console.log(typeof(nulla1))
