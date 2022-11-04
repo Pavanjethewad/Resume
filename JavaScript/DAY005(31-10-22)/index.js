fe53eba8a192d646e4c85ca6f6da582a94ce8bae
Pavanjethewad <pavanjethewad2@gmail.com>
2022-11-04 18:34:10 +0530

    commit

---
+++ JavaScript/DAY005(31-10-22)/index.js
@@ -0,0 +1,28 @@
+a = 95
+b = 95 
+if(a>b){
+    console.log("a is greater");
+}if(a<b){
+    console.log("b is greater");
+}else{
+    console.log(" numbers are equal ");
+}
+
+
+//
+var eng = 90
+var sci = 90
+var hist = 90
+var math = 90
+var hindi = 90
+
+var total = `${eng}`+ `${sci}` + `${hist}` + `${math}` + `${hindi}` 
+var total1 = eng + sci + hist + math + hindi
+var total12 = math + "" +sci
+var total13 = `${math}`
+
+console.log("total" , total);
+console.log("total" , total1);
+console.log("total" , total12);
+console.log("total" , total13);
+
