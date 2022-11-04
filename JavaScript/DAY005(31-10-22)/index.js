df74a9d1c50dc402907ab5dcfba53144c0cce791
Pavanjethewad <pavanjethewad2@gmail.com>
2022-11-04 19:25:39 +0530

    commit

--- JavaScript/DAY005(31-10-22)/index.js
+++ JavaScript/DAY005(31-10-22)/index.js
@@ -9,20 +9,42 @@ if(a>b){
 }
 
 
-//
+// //conditional else if else if
 var eng = 90
-var sci = 90
-var hist = 90
-var math = 90
-var hindi = 90
+var sci = 60
+var hist = 50
+var math = 95
+var hindi = 95
+var total = eng + sci + hist + math + hindi
+var percentage = (eng + sci + hist + math + hindi) /6
+// var total = `${eng}`+ `${sci}` + `${hist}` + `${math}` + `${hindi}` 
 
-var total = `${eng}`+ `${sci}` + `${hist}` + `${math}` + `${hindi}` 
-var total1 = eng + sci + hist + math + hindi
-var total12 = math + "" +sci
-var total13 = `${math}`
+// var total12 = math + "" +sci
+// var total13 = math , sci
+// var total14 = `${math}`
 
-console.log("total" , total);
-console.log("total" , total1);
-console.log("total" , total12);
-console.log("total" , total13);
+// console.log("total" , total);
+// console.log("total" , total1);
+// console.log("total" , total12);
+// console.log("total" , total13);
+// console.log("total" , total14);
 
+if(total > 90){
+    console.log("hey you get a+");
+}else if(total>80 && total<=90){
+    console.log("hey you get a ");
+}else if(total>70 && total<=80){
+    console.log("hey you get b+ ");
+}else if(total>60 && total<=70){
+    console.log("hey you get b ");
+}else if(total>50 && total<=60){
+    console.log("hey you get c+ ");
+}else if(total>40 && total<=50){
+    console.log("hey you get c ");
+}else{
+    console.log(" Le lag gayi Teri");
+}
+
+
+let suhanaMausam = false
+suhanaMausam ? console.log("mausam suhana hai") : console.log("chal jhoote")
